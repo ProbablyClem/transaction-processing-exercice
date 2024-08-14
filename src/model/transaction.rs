c814b87a65cfa5c52a7a0b793aaefefe2f4c8cb3
Clément Guiton <clement.guiton.dev@gmail.com>
2024-08-14 17:43:47 +0200

    Update transaction.rs

--- src/model/transaction.rs
+++ src/model/transaction.rs
@@ -1,5 +1,9 @@
 use super::transaction_type::TransactionType;
 
+// I usually like to make this an enum to enforce correctness with the type system
+// I would use the TransactionType as the variant and put the fields in the correct cases
+// This way, we could remove the option arount the amount and only specify it in the correct cases
+// Unforunately, the csv parser crate does not support tagged union https://github.com/BurntSushi/rust-csv/issues/211
 #[derive(Debug, serde::Deserialize, Clone)]
 pub struct Transaction {
     #[serde(rename = "type")]
